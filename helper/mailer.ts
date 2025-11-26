3ee38d8a2ff4219cb3bc3ac7141e24c1cbed8e33
MD Kamruzzaman Labib <MD Kamruzzaman>
2025-11-26 23:41:47 +0600

    nodemailer added code

---
+++ helper/mailer.ts
@@ -0,0 +1,30 @@
+import nodemailer from 'nodemailer';
+
+export const sendEmail = async({email, emailType, userId})=>{
+    try{
+        const transporter = nodemailer.createTransport({
+        host: "smtp.ethereal.email",
+        port: 587,
+        secure: false, // true for 465, false for other ports
+        auth: {
+        user: "maddison53@ethereal.email",
+        pass: "jn7jnAPss4f63QBp6D",
+         },
+});
+
+
+const mailOption = {
+    from: 'kamruzzamancse22@gmail.com',
+    to: email,
+    subject: emailType === 'VERIFY' ? "Verify your email" : 'Reset Your password',
+    text: "Hello world?", // plain‑text body
+    html: "<b>Hello world?</b>", // HTML body
+  }
+
+  const mailResponse =  await transporter.sendEmail(mailOption)
+  return mailResponse;
+
+    }catch(err){
+
+    }
+}
